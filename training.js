4ca53ba2b1a5d4b162888626c6f0a3c2d8566b57
Derrick Kwagala <derrick.kwagala@gmail.com>
2017-07-06 14:09:35 +0300

    minor change

--- training.js
+++ training.js
@@ -2,7 +2,7 @@
 // Using the example above. Given array ar1 = [1,2,3,4,5];, write a function which returns an array of functions functionsArray. 
 // Each item (Function) in functionsArray when executed should return the sums below.
 
-/* functionsArray[0]() = 1 // ar1[0]; 0 + 1
+/* functionsArray[0]() = 1 // ar1[0]; 0 + 1 
 functionsArray[1]() = 3 // ar1[0] + ar1[1]; // 1 + 2 = 3
 functionsArray[2]() = 5 // ar1[1] + ar1[2]; // 2 + 3 = 5
 functionsArray[3]() = 7 // ar1[2] + ar1[3]; // 3 + 4 = 7
