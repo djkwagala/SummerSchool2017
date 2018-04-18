e9bd44e5474cb5181c5088196a409f0ac8007a81
Derrick Kwagala <derrick.kwagala@gmail.com>
2018-04-18 19:57:34 +0300

    Completed Jasmine

--- jasmine/spec/AddressBookSpec.js
+++ jasmine/spec/AddressBookSpec.js
@@ -1,9 +1,20 @@
 describe("Address book", function () {
-    it("should be able to add a contact", function () {
-        var addressBook = new AddressBook();
-        var newContact = new Contact();
+    var addressBook, newContact;
 
+    beforeEach(function() {
+        addressBook = new AddressBook();
+        newContact = new Contact();
+    });
+
+    it("should be able to add a contact", function () {
         addressBook.addContact(newContact);
         expect(addressBook.getContact(0)).toBe(newContact);
-    })
+    });
+
+    it("should be able to delete a contact", function() {
+        addressBook.addContact(newContact);
+        addressBook.deleteContact(0);
+        expect(addressBook.getContact(0)).not.toBeDefined();
+        expect(addressBook.getContact(0)).toBe(undefined); // OR
+    });
 })
--- jasmine/src/AddressBook.js
+++ jasmine/src/AddressBook.js
@@ -1,21 +1,20 @@
 function AddressBook() {
-    var contactList;
+    var contactList = [];
     return {
         addContact: function (newContact) {
-
             if(this.contactList && this.contactList.length) {
                 this.contactList.push(newContact);
             } else {
                 this.contactList = [newContact];
             }
-
-            console.log(this.contactList)
-            console.log(newContact);
-
         },
         getContact: function(index) {
             return this.contactList[index];
+        },
+        deleteContact: function(index) {
+            this.contactList.splice(index,1);
         }
     }
-
 }
+
+// console.log(AddressBook.prototype);
